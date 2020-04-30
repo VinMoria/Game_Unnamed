ac318397cf0b5f190f947b91225c3dd499300004
Saturday <fanwei3638@live.com>
2020-04-30 20:12:32 +0800

    Singleton

---
+++ prototype1/Assets/script/Manager/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Singleton<T> where T : class, new()
+{
+    private static T instance = default(T);
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new T();
+            }
+
+            return instance;
+        }
+    }
+}
